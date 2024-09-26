5150979e04dc92d085a7806efe2b677f74b7675f
DevendraPPatil <devendra.patil@tekditechnologies.com>
2024-09-26 15:37:22 +0530

    Feat: Convert into fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-  console.log(`Server is running on the port ${process.env.PORT}`)
+  // Create a Fastify application
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  // Listen on the port defined in environment variables
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0'); 
+  console.log(`Server is running on port ${port}`);
 }
+
 bootstrap();
